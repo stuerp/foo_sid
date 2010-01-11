f92c389793efaf215543d69c68e8722b77c82542
Chris Moeller <kode54@gmail.com>
2010-01-11 08:59:11 +0000

    {11/20/2005 7:14:42 PM~11/20/2005 7:14:44 PM}
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection/]$/foobar2000/files/plugins.root;C50

--- sid.cpp
+++ sid.cpp
@@ -445,7 +445,12 @@ public:
 		return true;
 	}
 
-	bool decode_get_dynamic_info(file_info & p_out, double & p_timestamp_delta,bool & p_track_change)
+	bool decode_get_dynamic_info(file_info & p_out, double & p_timestamp_delta)
+	{
+		return false;
+	}
+
+	bool decode_get_dynamic_info_track(file_info & p_out, double & p_timestamp_delta)
 	{
 		return false;
 	}
